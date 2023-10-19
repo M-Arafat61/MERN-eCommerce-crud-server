cc1ce8b4332c0c7c295c4d83e6952760e3f7c828
M-Arafat61 <arafjust16@gmail.com>
2023-10-19 19:11:02 +0600

    get and put api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -28,8 +28,28 @@ async function run() {
     const apparelCollection = client.db("fashionDB").collection("apparel");
 
     const brandCollection = client.db("fashionDB").collection("Brands");
+    const cartCollection = client.db("fashionDB").collection("cart");
 
     // getting products from db
+    app.get("/products", async (req, res) => {
+      const cursor = apparelCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await apparelCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/cart", async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.get("/nike-products", async (req, res) => {
       const cursor = apparelCollection.find({ brand: "Nike" });
       const result = await cursor.toArray();
@@ -65,6 +85,44 @@ async function run() {
     app.post("/addProducts", async (req, res) => {
       const newProducts = req.body;
       const result = await apparelCollection.insertOne(newProducts);
+      res.send(result);
+    });
+
+    app.post("/userCart", async (req, res) => {
+      const cart = req.body;
+      const result = await cartCollection.insertOne(cart);
+      res.send(result);
+    });
+
+    app.put("/update-product/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+
+      const updateDoc = {
+        $set: {
+          image: updatedProduct.image,
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+        },
+      };
+
+      const result = await apparelCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
